d8a25f0355021925486ea52f2283cdcccff0f42f
Nikita Galushko <galnikrom@gmail.com>
2019-01-11 11:58:04 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,16 @@
 const path = require("path");
 const glob = require("glob");
 
+const entry = glob.sync("./src/*.js").reduce((prev, current) => {
+    return {
+        ...prev,
+        [path.basename(current).split(".js")[0]]: current
+    }
+}, {});
+
 module.exports = {
   mode: "development",
-  entry: glob.sync("./src/*.js"),
+  entry: entry,
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js"
